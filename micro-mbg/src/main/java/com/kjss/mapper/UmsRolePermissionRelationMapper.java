610c67a1a62132123666962d601c0088f45579f3
594021145 <549021145@qq.com>
2022-01-06 13:12:11 +0800

    generator code

---
+++ micro-mbg/src/main/java/com/kjss/mapper/UmsRolePermissionRelationMapper.java
@@ -0,0 +1,30 @@
+package com.kjss.mapper;
+
+import com.kjss.model.UmsRolePermissionRelation;
+import com.kjss.model.UmsRolePermissionRelationExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UmsRolePermissionRelationMapper {
+    long countByExample(UmsRolePermissionRelationExample example);
+
+    int deleteByExample(UmsRolePermissionRelationExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(UmsRolePermissionRelation record);
+
+    int insertSelective(UmsRolePermissionRelation record);
+
+    List<UmsRolePermissionRelation> selectByExample(UmsRolePermissionRelationExample example);
+
+    UmsRolePermissionRelation selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") UmsRolePermissionRelation record, @Param("example") UmsRolePermissionRelationExample example);
+
+    int updateByExample(@Param("record") UmsRolePermissionRelation record, @Param("example") UmsRolePermissionRelationExample example);
+
+    int updateByPrimaryKeySelective(UmsRolePermissionRelation record);
+
+    int updateByPrimaryKey(UmsRolePermissionRelation record);
+}
